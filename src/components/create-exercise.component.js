4b696c787e7e52db68260884c42642649fc1c198
Victor Yunis <andreyunis@gmail.com>
2023-02-06 23:55:04 +0100

    git ignore modified

---
+++ src/components/create-exercise.component.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class CreateExercise extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>You are on the Create Exercises component!</p>
+            </div>
+        );
+    }
+}
---
+++ src/components/create-user.component.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class CreateUser extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>You are on the Create User component!</p>
+            </div>
+        );
+    }
+}
