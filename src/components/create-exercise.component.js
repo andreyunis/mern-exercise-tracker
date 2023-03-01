2dc3906da793755466bae09151e2d9725d217d61
Victor Yunis <andreyunis@gmail.com>
2023-03-01 22:34:49 +0100

    Connect to axios the users in crate exercise user

--- src/components/create-exercise.component.js
+++ src/components/create-exercise.component.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import DatePicker from 'react-datepicker';
+import axios from 'axios';
 import "react-datepicker/dist/react-datepicker.css";
 
 export default class CreateExercise extends React.Component {
@@ -23,11 +24,15 @@ export default class CreateExercise extends React.Component {
     }
 
     componentDidMount() {
-        this.setState({
-            users: ['test user'],
-            username: 'test user',
-
-        })
+        axios.get('http://localhost:5000/users/')
+            .then(response => {
+                if (response.data.length > 0) {
+                    this.setState({
+                        users: response.data.map(user => user.username),
+                        username: response.data[0].username
+                    })
+                }
+            })
     }
 
     onChangeUsername(e) {
@@ -59,11 +64,14 @@ export default class CreateExercise extends React.Component {
 
         const exercise = {
             username: this.state.username,
-            description: this.state.duration,
+            description: this.state.description,
             duration: this.state.duration,
             date: this.state.date,
         }
         console.log(exercise);
+
+        axios.post('http://localhost:5000/exercises/add', exercise)
+            .then(res => console.log(res.data));
 
         window.location = '/';
     }
